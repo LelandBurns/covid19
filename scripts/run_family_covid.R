36e89e69c798a2bd822fb1d0db56b69c7675beda
Leland <leland@ip-172-18-1-134.ec2.internal>
2020-04-03 13:50:22 -0700

    re-format directories, clean up notebook, and add script to run cron

---
+++ scripts/run_family_covid.R
@@ -0,0 +1,38 @@
+### script for knitting Rmd file and sending email
+
+# load packages
+library(knitr)
+library(rJava)
+library(mailR)
+library(rmarkdown)
+
+# set-date
+today <- format(Sys.Date(), "%B %d, %Y")
+yesterday <- format(Sys.Date() - 1, "%B %d, %Y")
+
+# knit the file
+rmarkdown::render(input = "~/covid19/notebooks/covid_plots_fam.Rmd",
+                  output_format = "all",
+                  output_dir = "~/covid19/outputs/")
+
+# send email with attachments
+subject.text <- paste0("Automated COVID report for ", today)
+
+body.text <- paste0("This is an automated email from Leland. Attached is an html file with updated COVID data for our locations, with data included through ",
+                    yesterday, ". To view it, download and then click to open. It should open in your browser. If not, right-click it once downloaded and choose to open with ",
+                    "your browser of choice. If you have feedback or something else that you'd like included, let me know!")
+
+recipient.emails <- c("leland@ensemblex.com", "burnie310@yahoo.com")
+
+send.mail(from = "leland.b.burns@gmail.com",
+          to = recipient.emails,
+          subject = subject.text,
+          body = body.text,
+          smtp = list(host.name = "smtp.gmail.com",
+                      port = 465,
+                      user.name = Sys.getenv("personal_gmail_user"),
+                      passwd = Sys.getenv("personal_gmail_pass"),
+                      ssl = TRUE),
+          authenticate = TRUE,
+          send = TRUE,
+          attach.files = "~/covid19/outputs/covid_plots_fam.html")
