03541ac16ccfa0f325de920d4833f5afccf14a0b
Leland <leland@Ensemblex002.local>
2020-04-03 01:15:22 -0700

    Notebook updates and email function working.

--- run_family_covid.R
+++ run_family_covid.R
@@ -10,11 +10,10 @@ rmarkdown::render("~/covid19/covid_plots_fam.Rmd", "all")
 
 # try to send email with embedded html
 send.mail(from = "leland.b.burns@gmail.com",
-          to = c("leland@ensemblex.com", "burnie310@yahoo.com"),
-          subject = "Test of COVID HTML report",
-          body = "~/covid19/covid_plots_fam.html",
-          html = TRUE,
-          inline = TRUE,
-          smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "leland.b.burns@gmail.com", passwd = "8!8pr459D!cR", ssl = TRUE),
+          to = c("leland@ensemblex.com", "burnie310@yahoo.com", "alyssa.c.burns@gmail.com"),
+          subject = "I got the function to work!",
+          body = "I can send emails directly from where I work on data now. Attached is a version of the COVID data I've been working on, focused on our family. Download and open -- it should open in your browser.",
+          smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "leland.b.burns@gmail.com", passwd = "vuheumodsumkxgfb", ssl = TRUE),
           authenticate = TRUE,
-          send = TRUE)
+          send = TRUE,
+          attach.files = "~/covid19/covid_plots_fam.html")
