32cbeb3d96170901c6de25d5924107a87b4cd9ce
Leland <leland@ip-172-18-1-134.ec2.internal>
2020-04-09 21:45:45 -0700

    Add updates to report -- re-order and county rankings.

--- scripts/run_family_covid.R
+++ scripts/run_family_covid.R
@@ -15,7 +15,7 @@ yesterday <- format(Sys.Date() - 1, "%B %d, %Y")
 
 # knit the file
 rmarkdown::render(input = "~/covid19/notebooks/covid_plots_fam.Rmd",
-                  output_format = "all",
+                  output_format = "html_document",
                   output_dir = "~/covid19/outputs/")
 
 # send email with attachments
@@ -24,6 +24,10 @@ subject.text <- paste0("Automated COVID report for ", today)
 body.text <- paste0("This is an automated email from Leland. Attached is an html file with updated COVID data for our locations, with data included through ",
                     yesterday, ". To view it, download and then click to open. It should open in your browser. If not, right-click it once downloaded and choose to open with ",
                     "your browser of choice. If you have feedback or something else that you'd like included, let me know!")
+
+# body.text <- paste0("Attached is an html file with updated COVID data for our locations, with data included through ",
+#                     yesterday, ". Sorry, it's late today because I made some changes -- check them out! To view it, download and then click to open. It should open in your browser. If not, right-click it once downloaded and choose to open with ",
+#                     "your browser of choice. Schedule should return to normal tomorrow, with the report going out midday. If you have feedback or something else that you'd like included, let me know!")
 
 recipient.emails <- c("leland@ensemblex.com",
                       "alyssa.c.burns@gmail.com",
